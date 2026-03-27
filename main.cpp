588515014000cfddb80cea37114f9c28ff7449da
BATMINJI <minjiconan@naver.com>
2026-03-27 15:01:26 +0900

    Sort 함수 실습

--- main.cpp
+++ main.cpp
@@ -2,7 +2,48 @@
 #include "Engine.h"
 #include "World.h"
 
-void Sort(int* InData, int Size)
+int Ascending(const void* A, const void* B)
+{
+	if (*(int*)A - *(int*)B > 0)
+		return 1;
+	if (*(int*)A - *(int*)B < 0)
+		return -1;
+	return 0;
+}
+
+int Desending(const void* A, const void* B)
+{
+	if (*(int*)A - *(int*)B > 0)
+		return -1;
+	if (*(int*)A - *(int*)B < 0)
+		return 1;
+	return 0;
+}
+
+void Swap(void* A, void* B, size_t Size)
+{
+	std::vector<char> Temp(Size);
+	std::memcpy(Temp.data(), A, Size);
+	std::memcpy(A, B, Size);
+	std::memcpy(B, Temp.data(), Size);
+}
+
+void Sort(void* InData, size_t Count, size_t Size, int (*Compare)(const void*, const void*))
+{
+	char* Ptr = (char*)InData;
+	for (int i = 0; i < Count - 1; ++i)
+	{
+		for (int j = i + 1; j < Count; ++j)
+		{
+			if (Compare((Ptr + Size * i), (Ptr + Size * j)) > 0)
+			{
+				Swap((Ptr + Size * i), (Ptr + Size * j), Size);
+			}
+		}
+	}
+}
+
+void SelectionSort(int* InData, int Size)
 {
 	for (int i = 0; i < Size; ++i)
 	{
@@ -28,7 +69,9 @@ int main()
 
 	int Data[10] = { 9, 1, 3, 5, 4, 6, 7, 8, 2, 10 };
 	
-	Sort(Data, 10);
+	// SelectionSort(Data, 10);
+	// Sort(Data, 10, sizeof(int), Ascending);
+	Sort(Data, 10, sizeof(int), Desending);
 
 	for (int i = 0; i < 10; ++i)
 	{
